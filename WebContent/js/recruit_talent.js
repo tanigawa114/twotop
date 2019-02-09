84176e21c8aa8e4b1f2b8403747b98a6289a8722
ueki <ueki@TWOTOP904>
2019-02-09 13:08:25 +0900

    修正と分割化

---
+++ WebContent/js/recruit_talent.js
@@ -0,0 +1,13 @@
+// @author Ueki
+
+$(function() {
+	$("#rercuit").load("recruit_company.html");
+});
+
+$(function() {
+	$("#header").load("header.html");
+});
+
+$(function() {
+	$("#footer").load("footer.html");
+});
